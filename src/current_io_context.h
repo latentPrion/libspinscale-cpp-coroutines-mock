a26603dda36af5a37ebac08c0746d3d3b34c852f
Hayodea Hekol <hayodea@protonmail.com>
2026-05-18 03:59:23 -0400

    Delete these (now) superfluous files

--- src/current_io_context.h
+++
@@ -1,8 +0,0 @@
-#ifndef CURRENT_IO_CONTEXT_H
-#define CURRENT_IO_CONTEXT_H
-
-#include <boost/asio/io_context.hpp>
-
-boost::asio::io_context &current_io_context();
-
-#endif // CURRENT_IO_CONTEXT_H
